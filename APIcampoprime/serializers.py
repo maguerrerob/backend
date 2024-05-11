4c849e0261e592ccc96657aac29f30baf7ec4bb3
maguerrerob <manuel.guerrero.boza.al@iespoligonosur.org>
2024-05-11 22:13:02 +0200

    login y registro

--- APIcampoprime/serializers.py
+++ APIcampoprime/serializers.py
@@ -15,3 +15,24 @@ class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = "__all__"
+
+class UsuarioSerializerRegistro(serializers.Serializer):
+    username = serializers.CharField()
+    password1 = serializers.CharField()
+    password2 = serializers.CharField()
+    email = serializers.EmailField()
+    telefono = serializers.CharField(max_length=9)
+    rol = serializers.IntegerField()
+
+    def validate_username(self, username):
+        usuario = Usuario.objects.filter(username=username).first()
+        if (not usuario is None):
+            raise serializers.ValidationError("Ya existe un usuario con eso nombre")
+        return username
+    
+    def validate_password1(self, password1):
+        data = self.get_initial()
+        password2 = data.get('password2')
+        if password1 != password2:
+            raise serializers.ValidationError("Las contraseñas no coinciden")
+        return password1
--- APIcampoprime/views.py
+++ APIcampoprime/views.py
@@ -4,6 +4,15 @@ from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from oauth2_provider.models import AccessToken
+# Para registro
+from rest_framework import generics
+# Para permitir todo el acceso a la vista
+from rest_framework.permissions import AllowAny
+# Para usar grupos de Django
+from django.contrib.auth.models import Group
+# Para indicar errores (500, 400, 401, etc)
+from rest_framework import status
+
 
 # Create your views here.
 
@@ -28,3 +37,38 @@ def obtener_usuario_token(request, token):
         return Response(serializer.data)
     except AccessToken.DoesNotExist:
         return None
+    
+class registrar_usuario(generics.CreateAPIView):
+    serializer_class = UsuarioSerializerRegistro
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializers = UsuarioSerializerRegistro(data=request.data)
+        if serializers.is_valid():
+            try:
+                rol = request.data.get('rol')
+                user = Usuario.objects.create_user(
+                    username = serializers.data.get('username'),
+                    email = serializers.data.get('email'),
+                    password = serializers.data.get('password1'),
+                    rol = rol
+                )
+                if (rol == str(Usuario.CLIENTE)):
+                    grupo = Group.objects.get(name='cliente')
+                    grupo.user_set.add(user)
+                    telefono = serializers.validated_data.get('telefono')
+                    cliente = Cliente.objects.create(usuario=user, telefono=telefono)
+                    cliente.save()
+                elif (rol == str(Usuario.DUENYO_RECINTO)):
+                    grupo = Group.objects.get(name='duenyo_recinto')
+                    grupo.user_set.add(user)
+                    telefono = serializers.validated_data.get('telefono')
+                    duenyo_recinto = Duenyo_recinto.objects.create(usuario=user, telefono=telefono)
+                    duenyo_recinto.save()
+                usuarioSerializado = UsuarioSerializer(user)
+                return Response(usuarioSerializado.data)
+            except Exception as error:
+                print(repr(error))
+                return Response(repr(error), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
